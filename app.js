9cb5d6463101513ff97184c163e831fcc41bb5d2
Henok-Enyew <henokenyew86@gmail.com>
2024-11-12 21:44:14 +0300

    UPDATE | Authentication Added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 
 const AppError = require(`./utils/AppError`);
@@ -7,15 +12,53 @@ const globalErrorHandler = require(`./controllers/errorController`);
 
 const userRouter = require(`./routes/userRoutes`);
 const tourRouter = require(`./routes/tourRoutes`);
+const { whitelist } = require('validator');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// 1.GLOBAL MIDDLEWARES
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from the same API
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many request from this ip please try again in an hour.',
+});
+
+app.use('/api', limiter);
+
+// Body parser reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanotization against NOSQL query injection
+app.use(mongoSanitize());
+// Data sanotization against XSS
+app.use(xss());
+
+//  Prevent paramter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,18 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   console.log(user._id);
   const token = signToken(user._id);
-  res.cookies('jwt', token);
+
+  const cookieOptions = {
+    expiresIn: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+
   return res.status(statusCode).json({
     status: 'success',
     token,
